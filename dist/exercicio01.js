913e49cc509bd2226158ceaf9e1da43bab98441d
Marisol Poma <marisol.poma15@hotmail.com>
2019-08-02 20:31:48 -0300

    Exercicio01 -Typescript

---
+++ dist/exercicio01.js
@@ -0,0 +1,11 @@
+var nota1 = 5;
+var nota2 = 5;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média é" + media);
+if (media >= 7) {
+    console.log("APROVADO");
+}
+else {
+    console.log("REPROVADO");
+}
---
+++ exercicio01.ts
@@ -0,0 +1,13 @@
+let nota1: number = 5 
+let nota2: number = 5
+let nota3: number = 5
+
+let media: number = (nota1 + nota2 + nota3) / 3
+
+console.log ("A média é" + media)
+
+if(media>=7) {
+    console.log("APROVADO")
+}else{
+    console.log("REPROVADO")
+} 
