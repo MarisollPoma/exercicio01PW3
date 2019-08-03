2c30909ef4f85f08c81ad51eec0aceeb7d5519f3
Marisol Poma <marisol.poma15@hotmail.com>
2019-08-02 21:51:20 -0300

    Exercicio1 pwiii

---
+++ dist/exercicio02.js
@@ -0,0 +1,10 @@
+var Competidores = 10;
+var Qtdefolhas = 99;
+var FolhaPcompetidor = 10;
+var quantidadefolha = (Competidores * FolhaPcompetidor);
+if (quantidadefolha > Qtdefolhas) {
+    console.log("NÃO");
+}
+else {
+    console.log("SIM");
+}
---
+++ exercicio02.ts
@@ -0,0 +1,11 @@
+let Competidores: number = 10
+let Qtdefolhas: number = 99
+let FolhaPcompetidor: number = 10
+
+let quantidadefolha: number = (Competidores * FolhaPcompetidor)
+
+if (quantidadefolha>Qtdefolhas) {
+    console.log("NÃO")
+}else {
+    console.log("SIM")
+}
